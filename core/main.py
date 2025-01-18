23e8a58b20fc9c80c5ee43b2d334efccd1b4f0ef
aalyabd <aaly.abedy@gmail.com>
2025-01-18 21:20:04 +0330

    type a string if input text

--- core/main.py
+++ core/main.py
@@ -5,4 +5,8 @@ API_TOKEN = os.environ.get("API_TOKEN")
 
 bot = telebot.TeleBot(API_TOKEN)
 
+@bot.message_handler(content_types=['text'])
+def send_message(message):
+    bot.send_message(message.chat.id,'this is a text')
+
 bot.infinity_polling()
